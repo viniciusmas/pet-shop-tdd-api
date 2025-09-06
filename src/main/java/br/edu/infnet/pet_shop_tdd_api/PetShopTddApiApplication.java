1dc3381b02cba7a20285a80ebeaf0c162d41480f
Vinicius M A Souza <vinicius.moreira.souza@fazenda.rj.gov.br>
2025-09-06 19:38:59 -0300

    Commit inicial

---
+++ src/main/java/br/edu/infnet/pet_shop_tdd_api/PetShopTddApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.pet_shop_tdd_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopTddApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopTddApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/pet_shop_tdd_api/PetShopTddApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.pet_shop_tdd_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopTddApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
