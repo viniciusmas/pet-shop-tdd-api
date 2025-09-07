d3c84450a2f301c8975121fe72eeeec00678d291
Vinicius M A Souza <vinicius.moreira.souza@fazenda.rj.gov.br>
2025-09-07 19:57:52 -0300

    Criação do método agendar

--- src/main/java/br/edu/infnet/petshoptddapi/service/AgendamentoService.java
+++ src/main/java/br/edu/infnet/petshoptddapi/service/AgendamentoService.java
@@ -1,4 +1,10 @@
 package br.edu.infnet.petshoptddapi.service;
 
+import br.edu.infnet.petshoptddapi.domain.Agendamento;
+
 public class AgendamentoService {
+
+    public String agendar(Agendamento agendamento) {
+        throw new UnsupportedOperationException("Método agendar ainda não foi implementado.");
+    }
 }
